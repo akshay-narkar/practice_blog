51d321d1c349f07895a3fb612929ef5bc505a097
akshay-narkar <akshaynarkar1295@gmail.com>
2021-01-15 14:57:54 +0530

    Create students controller, students rresource and form for the same

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,4 +1,7 @@
 class StudentsController < ApplicationController
 def new
 end
+def create
+    render plain: params[:student11].inspect
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'welcome/index'
-resources :students
   root 'welcome#index'
-  
+  resources :students
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
