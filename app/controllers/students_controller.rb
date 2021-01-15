aeb5cda583de55399e520a835953f44d68584412
akshay-narkar <akshaynarkar1295@gmail.com>
2021-01-15 16:21:51 +0530

    Edit show and create actions. templates added to display blog

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,6 +2,18 @@ class StudentsController < ApplicationController
 def new
 end
 def create
-    render plain: params[:student11].inspect
+    # render plain: params[:student11].inspect
+    @student12 = Student.new(student_params)
+    @student12.save
+    redirect_to @student12
+end
+
+def show
+    @student12 = Student.find(params[:id])
+end
+
+private 
+def student_params
+    params.require(:student11).permit(:title,:text)
 end
 end
